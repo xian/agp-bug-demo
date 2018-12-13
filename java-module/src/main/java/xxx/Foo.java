857bf18798f1b114cea7944319e0381d752e3b1f
Christian Williams <christianw@google.com>
2018-12-12 19:44:44 -0800

    Initial commit.

---
+++ java-module/src/main/java/xxx/Foo.java
@@ -0,0 +1,11 @@
+package xxx;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+public class Foo {
+  public static void main(String[] args) {
+    System.out.println(AndroidJUnit4.class);
+    System.out.println(InstrumentationRegistry.class);
+  }
+}
